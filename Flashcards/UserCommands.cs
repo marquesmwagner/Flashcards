dadc89d5e702a81f8305d823db220d18a293e39f
marquesmwagner <marquesmwagner@gmail.com>
2023-01-03 16:25:19 -0300

    Add StacksMenu method.

--- Flashcards/UserCommands.cs
+++ Flashcards/UserCommands.cs
@@ -11,12 +11,12 @@ namespace Flashcards
         internal static void MainMenu()
         {
             bool closeApp = false;
-            while(!closeApp)
+            while(closeApp == false)
             {
                 Console.WriteLine("\n\nMAIN MENU");
                 Console.WriteLine("\nWhat you would like to do?");
-                Console.WriteLine("\n0 - Close Application");
-                Console.WriteLine("\n1 - Manage Flashcards");
+                Console.WriteLine("\n0 - Close application");
+                Console.WriteLine("\n1 - Manage flashcards");
                 Console.WriteLine("\n2 - Study");
 
                 string commandInput = Console.ReadLine();
@@ -34,13 +34,55 @@ namespace Flashcards
                         closeApp = true;
                         break;
                     case 1:
-                        //Call StacksMenu();
+                        StacksMenu();
                         break;
                     case 2:
                         //Call StudyMenu();
                         break;
                     default:
-                        Console.WriteLine()
+                        Console.WriteLine("\nInvalid choice. Please enter a valid input.\n");
+                        break;
+                }
+            }
+        }
+
+        public static void StacksMenu()
+        {
+            Console.WriteLine("\n\nFlashCard Stacks Area\n");
+            //StacksController.GetStacks();
+
+            bool closeArea = false;
+            while(closeArea == false)
+            {
+                Console.WriteLine("\nWhat you would like to do?");
+                Console.WriteLine("\n0 - Close application");
+                Console.WriteLine("\n1 - Return to main menu");
+                Console.WriteLine("\n2 - Create new stack");
+                Console.WriteLine("\n3 - Manage a stack");
+
+                string commandInput = Console.ReadLine();
+                while (string.IsNullOrEmpty(commandInput) || !int.TryParse(commandInput, out _))
+                {
+                    Console.WriteLine("\nInvalid choice. Please enter a valid input");
+                    commandInput = Console.ReadLine();
+                }
+
+                int command = Convert.ToInt32(commandInput);
+
+                switch (command)
+                {
+                    case 0:
+                        closeArea = true;
+                        break;
+                    case 1:
+                        //StacksController.CreateStack();
+                        break;
+                    case 2:
+                        //StacksControle.ManageStack();
+                        break;
+                    default:
+                        Console.WriteLine("\nInvalid choice. Please enter a valid input.");
+                        break;
                 }
             }
         }
