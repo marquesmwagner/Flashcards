bd0f22a62965be926162c7d7dd2e2f71fa1a1b2c
marquesmwagner <marquesmwagner@gmail.com>
2023-01-03 10:51:12 -0300

    Add CheckDatabase method.

--- Flashcards/DatabaseManager.cs
+++ Flashcards/DatabaseManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,31 @@ namespace Flashcards
 {
     internal class DatabaseManager
     {
+        internal static void CheckDatabase()
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "Server=(LocalDb)\\MSSQLLocalDB";
+
+                using (SqlConnection connection = new SqlConnection(builder.DataSource))
+                {
+                    connection.Open();
+                    String sql = @"IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'FlashCardsDB')
+                                   BEGIN
+                                   CREATE DATABASE FlashCardsDB;
+                                   END";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    connection.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
