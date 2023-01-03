af06d5324d16c12b5ac8ef770e5474da40f7568c
marquesmwagner <marquesmwagner@gmail.com>
2023-01-03 12:58:59 -0300

    Add StudySession Class.

---
+++ Flashcards/Models/StudySession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards.Models
+{
+    internal class StudySession
+    {
+        public int Id { get; set; }
+        public string StackName { get; set; }
+        public int NumberOfQuestions { get; set; }
+        public int Score { get; set; }
+    }
+}
